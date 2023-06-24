ad4430093bb52e79796f7247a308a149e73cc635
mcruz100 <mdcruz100@gmail.com>
2023-06-24 15:25:08 -0400

    Visual Changes to NameEntry also added a few files

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© 2023 Spotlight. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
